385acec62c8527427b288126afc30690892874ae
FDelfim <lipedelfim@gmail.com>
2023-07-19 23:17:20 -0300

    feat: update .env + about section

---
+++ pages/about.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Layout from './../components/Layout';
+import { Flex, Text } from '@chakra-ui/react';
+
+export default function about() {
+  return (
+    <Layout>
+      <Flex justifyContent='center' alignItems='center'>
+        <Text fontSize='4xl' mt='10'>🚧 Em breve 🚧</Text>
+      </Flex>
+    </Layout>
+  )
+}
