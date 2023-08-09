57aac612e73dec1148a8f0e46fbb7780cebc0793
FDelfim <lipedelfim@gmail.com>
2023-08-08 21:28:47 -0300

    style: update fav icon + title

--- components/Layout.js
+++ components/Layout.js
@@ -1,15 +1,21 @@
 import { Box, Flex, useColorModeValue } from '@chakra-ui/react';
 import NavBar from './nav';
+import Head from 'next/head';
 
 function Layout({ children }) {
   const bgColor = useColorModeValue('gray.50', 'gray.900');
   return (
-    <Box bgColor={bgColor} minH='100vh' m='0'>
-      <NavBar />
-      <Flex flexDirection='column' w='100%' h='100%' pt='62px'>
-        {children}
-      </Flex>
-    </Box>
+    <>
+      <Head>
+        <title>Resiliência Psicológica no Esporte</title>
+      </Head>
+      <Box bgColor={bgColor} minH='100vh' m='0'>
+        <NavBar />
+        <Flex flexDirection='column' w='100%' h='100%' pt='62px'>
+          {children}
+        </Flex>
+      </Box>
+    </>
   );
 }
 
